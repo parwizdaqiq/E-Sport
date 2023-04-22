4cad836d2281f7d369d4b7019b7e24f095320451
parwizdaqiq <daqiqparwiz@gmail.com>
2023-04-23 04:31:02 +0500

    Completed the Login page modal

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,11 +1,40 @@
 import { Injectable } from '@angular/core';
 
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
-  visible = false
+  private modals: IModal[] = []
 
-  constructor() {}
+  constructor() { }
 
+  register(id: string) {
+    this.modals.push({
+      id,
+      visible: false
+    })
+  }
+
+  unregister(id: string) {
+    this.modals = this.modals.filter (
+      element => element.id !== id
+    )
+  }
+   
+  isModalOpen(id: string) : boolean {
+    return !!this.modals.find(element => element.id === id)?.visible
+  }
+
+  toggleModal(id: string) {
+    const modal = this.modals.find(element => element.id === id)
+
+    if(modal) {
+      modal.visible = !modal.visible
+    }
+  }
 }
