0f4cb97950caac08fdb6dcc854a257f93d10c132
MacBook Pro <macbookpro@MacBooks-MBP.bbrouter>
2024-01-31 23:31:02 +0430

    set up firebase

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,10 @@ import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  constructor( private auth: AngularFireAuth ) { }
+
+  inSubmission = false
+
   name = new FormControl('', [
     Validators.required,
     Validators.minLength(3)
@@ -46,9 +51,29 @@ export class RegisterComponent {
     phoneNumber: this.phoneNumber
 
   })
-  register() {
+
+  async register() {
     this.showAlert = true
     this.alertMsg = 'Please Wait! Your account is being created. '
     this.alertColor = 'blue'
-  }
+    this.inSubmission = true
+
+    const { email, password } = this.registerForm.value
+
+    try {
+      const userCred = await this.auth.createUserWithEmailAndPassword(
+      email as string, password as string 
+      )
+      console.log(userCred)
+    } catch(e) {
+      console.error(e)
+
+      this.alertMsg = 'An unexpected error occurred. please try again later!'
+      this.alertColor = 'red'
+      return
+    }   
+    this.alertMsg = 'Success! Your Account has been created'
+    this.alertColor = 'green'
+  } 
+
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyDAcp8Kq1tl4wmCY2cqm2IqAzpqC_ax9u8",
+        authDomain: "e-sport-1cd26.firebaseapp.com",
+        projectId: "e-sport-1cd26",
+        storageBucket: "e-sport-1cd26.appspot.com",
+        appId: "1:1069737667174:web:1ba9bf79bac0e759b61ba7"
+      }
+  };
