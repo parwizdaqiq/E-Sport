326e88b4a6dc250cc0430e3a0d637c10dd3a941d
MacBook Pro <macbookpro@MacBooks-MBP.bbrouter>
2024-02-23 00:38:19 +0430

    added collection types

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import IUser from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private userCollection: AngularFirestoreCollection<IUser> 
+
+  constructor(
+    private auth: AngularFireAuth,
+    private db: AngularFirestore
+  ) { 
+    this.userCollection = db.collection('users')
+  }
+  public async createUser(userData:IUser) {
+    if (!userData.password) {
+      throw new Error("Password not provided!")
+    }
+    const userCred = await this.auth.createUserWithEmailAndPassword(
+      userData.email as string, userData.password as string 
+      )
+
+      await this.userCollection.add({
+        name: userData.name,
+        email: userData.email,
+        age: userData.age,
+        phoneNumber: userData.phoneNumber
+      })
+  }
+}
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AuthService } from 'src/app/services/auth.service';
+import IUser from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-register',
@@ -10,8 +10,7 @@ import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/compat/f
 })
 export class RegisterComponent {
   constructor( 
-    private auth: AngularFireAuth,
-    private db: AngularFirestore
+    private auth: AuthService
      ) {}
 
   inSubmission = false
@@ -24,7 +23,7 @@ export class RegisterComponent {
     Validators.required,
     Validators.email
   ])
-  age = new FormControl('', [
+  age = new FormControl<number | null>(null, [
     Validators.required,
     Validators.min(18),
     Validators.max(120)
@@ -62,18 +61,10 @@ export class RegisterComponent {
     this.alertColor = 'blue'
     this.inSubmission = true
 
-    const { email, password } = this.registerForm.value
+ 
 
     try {
-      const userCred = await this.auth.createUserWithEmailAndPassword(
-      email as string, password as string 
-      )
-      await this.db.collection('users').add({
-        name: this.name.value,
-        email: this.email.value,
-        age: this.age.value,
-        phoneNumber: this.phoneNumber.value
-      })
+      await this.auth.createUser(this.registerForm.value as IUser)
 
     } catch(e) {
       console.error(e)
